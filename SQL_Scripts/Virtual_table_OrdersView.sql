1f26fe6982bee7a814df471b6d4bfea6ce14cd6c
Perfect_Envoy <61292951+PerfectEnvoyRG@users.noreply.github.com>
2023-03-18 06:51:09 +0800

    Delete Virtual_table_OrdersView.sql

--- SQL_Scripts/Virtual_table_OrdersView.sql
+++
@@ -1,6 +0,0 @@
-USE little_lemon_db;
-DROP VIEW IF EXISTS OrdersView;orderviewquantity
-CREATE VIEW OrderView AS
-		SELECT order_id, quantity, total_cost
-        FROM orders
-        WHERE quantity > 2;
